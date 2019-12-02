01bf4e4f98fcda1b1ea7e14d8b1d0cbdd071dccb
Christnovie <48351212+Christnovie@users.noreply.github.com>
2019-12-02 16:00:18 +0100

    added Exercice repository

---
+++ ExerciceGit/ArraysString/Arrays_String.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ *Projet  :Arrays String
+ *Auteur : christnovie kiala binga
+ *Version : 1.0
+ *Date : 02/12/2019
+ */
+?>
+<!DOCTYPE html >
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Date</title>
+    <link rel="stylesheet" type="text/css" href="">
+</head>
+<body>
+<div id="">
+    <div class="">
+        <?php
+
+
+
+        ?>
+    </div>
+</div>
+</body>
+
+</html>
