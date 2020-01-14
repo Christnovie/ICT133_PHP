ebd5bc5cfcb05f779df58d78b7cceddd5065df14
chris <christnovie.kiala-binga@cpnv.ch>
2020-01-14 11:38:22 +0100

    read/write into json file

--- Site/Modele/model.php
+++ Site/Modele/model.php
@@ -10,7 +10,7 @@
 
 /**
  * function for check a validity of password
- * @param $password
+ * @param $password * user login input data
  * @return bool
  */
 
@@ -18,24 +18,24 @@ function checklogin($password)
 {
     $current_file = file_get_contents("Modele/userDatabase.json");
     $parsed = json_decode($current_file);
-    foreach ($parsed as $item => $val) {
-        if ($parsed[$val]->User == @$password['inputUsername']) {
-            $i = $val;
+    foreach ($parsed as $item => $value) {
+        if ($value->User == @$password['inputUsername']) {
+            if (@$password['pwd'] == $value->Password && @$password['inputUsername'] == $value->User)
+                return true;
+            else
+                return false;
+
         }
     }
 
 
-    if (@$password['pwd'] == $parsed[$i]->Password && @$password['inputUsername'] == $parsed[$i]->User)
-        return true;
-    else
-        return false;
 
 
 }
 
 /**
  * function for create a user and write to json file the user data
- * @param $userData
+ * @param $userData /*user register data name + password
  */
 function creatUser($userData)
 {
