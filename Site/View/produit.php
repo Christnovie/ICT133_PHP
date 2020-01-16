7b368f5e7ca6273669c937e21550ad5e530b39c7
chris <christnovie.kiala-binga@cpnv.ch>
2020-01-16 17:03:55 +0100

    aa

--- Site/View/produit.php
+++ Site/View/produit.php
@@ -10,15 +10,16 @@
 ob_start();
 $titre = "Rent a show - Acceuil";
 ?>
+<link href="View/Content/script/bootstrap/css/boostrapShop" rel="stylesheet" type="text/css">
     <h1> Snowboard shop </h1>
-    <div class="table">
+    <div class="table table-hover  table-condensed">
         <table >
             <tr>
 
                 <?php
                 foreach ($_GET['prContent'] as $item) {
                     foreach ($item as $value) {
-                        echo '<td><a href=""><img src=" ' . $value->image . '  ">', " <br> ", $value->name, "", "</a></td>               ";
+                        echo '<td ><a href=""><img src=" ' . $value->image . '  ">', " <br> ", $value->name, "", "</a></td>               ";
 
                     }
                 }
